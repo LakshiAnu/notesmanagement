607ce132233269587338f3380b860e923af588b8
LakshiAnu <lakshianuradhi@gmail.com>
2022-07-04 13:22:22 +0530

    Register Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
