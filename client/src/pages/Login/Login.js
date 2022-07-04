391119c0db71b0924ca9927a1df304baab206e9b
LakshiAnu <lakshianuradhi@gmail.com>
2022-07-04 13:22:36 +0530

    Page formatted

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -8,23 +8,23 @@ function Login() {
             <div className="Login">
                 <div className="col-md-6 login-container">
                     <div className="sign-container col-12">
-                    <h2>Sign In</h2>
-                    <form className="text-start" style={{marginTop: '5rem'}}>
-                        <div className="mb-3">
-                        <label htmlFor="email">Email Address</label>
-                        <input type="email" className="form-control" id="email" placeholder="name@example.com" />
-                        </div>
-                        <div className="mb-3">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="***********" />
-                        </div>
-                        <div class="d-grid gap-2 mt-5">
-                        <button className="btn btn-success btn-lg">Sign In</button>
-                        </div>
-                        <div className="mt-3">
-                        <p>Don't have an account yet? <Link to="/register" className="register">Register</Link></p>
-                        </div>
-                    </form>
+                        <h2>Sign In</h2>
+                        <form className="text-start" style={{marginTop: '5rem'}}>
+                            <div className="mb-3">
+                            <label htmlFor="email">Email Address</label>
+                            <input type="email" className="form-control" id="email" placeholder="name@example.com" />
+                            </div>
+                            <div className="mb-3">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" id="password" placeholder="***********" />
+                            </div>
+                            <div class="d-grid gap-2 mt-5">
+                            <button className="btn btn-success btn-lg">Sign In</button>
+                            </div>
+                            <div className="mt-3">
+                            <p>Don't have an account yet? <Link to="/register" className="register">Register</Link></p>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
