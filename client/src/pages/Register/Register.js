5bd39cf6c9d836f6cbcfc317e44f43a42666a975
LakshiAnu <lakshianuradhi@gmail.com>
2022-07-04 20:39:19 +0530

    Added the useState hook for inputs

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Register.css';
 import {Link} from 'react-router-dom';
 
 function Register() {
+    
+    const [inpvalue, setInpvalue] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        mobile: "",
+        type: "",
+        dob: "",
+    });
+    const setData = (e) => {
+        const {name, value} = e.target;
+        setInpvalue((preval)=>{
+            return {
+                ...preval,
+                [name]: value
+            }
+        });
+    }
     return (
     <div className="container">
         <div className="Register">
@@ -12,19 +31,20 @@ function Register() {
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label htmlFor="firstName">First Name</label>
-                            <input type="firstName" className="form-control" id="firstName" placeholder="Jane" required />
+                            <input type="firstName" className="form-control" id="firstName" name="firstName" placeholder="Jane" required value={inpvalue.firstName} onChange={setData} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="email">Email Address</label>
-                            <input type="email" className="form-control" id="email" placeholder="name@example.com" required />
+                            <input type="email" className="form-control" id="email" name="email" placeholder="name@example.com" required value={inpvalue.email} onChange={setData} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="mobile">Mobile</label>
-                            <input type="number" className="form-control" id="mobile" placeholder="94xxxxxxxxx" required />
+                            <input type="number" className="form-control" id="mobile" name="mobile" placeholder="94xxxxxxxxx" required value={inpvalue.mobile} onChange={setData} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="type">User Type</label>
-                            <select id="type" className="form-select" required>
+                            <select id="type" name="type" className="form-select" required value={inpvalue.type} onChange={setData}>
+                                <option value="">Select User Type</option>
                                 <option value="admin">Admin</option>
                                 <option value="user">User</option>
                             </select>
@@ -33,15 +53,15 @@ function Register() {
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label htmlFor="lastName">Last Name</label>
-                            <input type="lastName" className="form-control" id="lastName" placeholder="Doe" required />
+                            <input type="lastName" className="form-control" id="lastName" name="lastName" placeholder="Doe" required value={inpvalue.lastName} onChange={setData} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="dob">Date of Birth</label>
-                            <input type="date" className="form-control" id="dob" required />
+                            <input type="date" className="form-control" id="dob" name="dob" required value={inpvalue.dob} onChange={setData} />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="password">Password</label>
-                            <input type="password" className="form-control" id="password" placeholder="***********" required />
+                            <input type="password" className="form-control" id="password" name="password" placeholder="***********" required value={inpvalue.password} onChange={setData} />
                         </div>
                     </div>
                     <div class="mt-4 text-start">
