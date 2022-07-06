2925e4c57dfeb9f987069ad13b459d0cd5238208
LakshiAnu <lakshianuradhi@gmail.com>
2022-07-06 16:19:10 +0530

    Note list and create page add

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {Routes, Route} from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Userlist from './pages/Userlist/Userlist';
+import Noteslist from './pages/Noteslist/Noteslist';
+import Notescreate from './pages/Notescreate/Notescreate';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/userlist" element={<Userlist />} />
+        <Route path="/noteslist" element={<Noteslist />} />
+        <Route path="/notescreate" element={<Notescreate />} />
       </Routes>
     </div>
   );
