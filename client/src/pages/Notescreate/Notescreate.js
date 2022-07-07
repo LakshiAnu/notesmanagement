3b328f45d34f8d83ace732e26c610c6617bb41b7
LakshiAnu <lakshianuradhi@gmail.com>
2022-07-07 15:21:09 +0530

    Notes create function completed

--- client/src/pages/Notescreate/Notescreate.js
+++ client/src/pages/Notescreate/Notescreate.js
@@ -1,12 +1,36 @@
 import React, {useState} from 'react'
+import {useParams, useNavigate, Link} from 'react-router-dom';
 import "./Notescreate.css";
 
 function Notescreate() {
+    const params = useParams();
+    const navigate = useNavigate();
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
 
-    const addNote = async() => {
+    const addNote = async(e) => {
+        e.preventDefault();
+        const user = params.id;
 
+        const res = await fetch("http://localhost:8003/createNote",{
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                title, description, user
+            })
+        })
+
+        const data = await res.json();
+
+        if(res.status === 422 || !data){
+            alert("Error: "+data);
+            console.log(data);
+        }else{
+            alert("Successfully added");
+            navigate(`/noteslist/${user}`);
+        }
     }
     return (
         <div className="container">
@@ -21,9 +45,10 @@ function Notescreate() {
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="txtDescription">Description</label>
-                                <textarea name="txtDescription" id="txtDescription" rows="10" placeholder="Description" className="form-control" onChange={(e)=>setDescription(e.target.value)}>{description}</textarea>
+                                <textarea name="txtDescription" id="txtDescription" rows="10" placeholder="Description" className="form-control" onChange={(e)=>setDescription(e.target.value)} value={description}></textarea>
                             </div>
                             <div className="mt-2 text-start">
+                                <Link to={`/noteslist/${params.id}`} className="btn btn-warning btn-lg me-1">Back</Link>
                                 <button className="btn btn-success btn-lg" onClick={addNote}>Save</button>
                             </div>
                         </div>
